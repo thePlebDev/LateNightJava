6cb8259fe5b6137bcc56a01bc9795586599fd842
Tristan Elliott <>
2022-05-21 21:51:44 -0300

    initial commit

---
+++ src/main/java/com/example/LateNightJava/LateNightJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.LateNightJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LateNightJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LateNightJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/LateNightJava/LateNightJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LateNightJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LateNightJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
