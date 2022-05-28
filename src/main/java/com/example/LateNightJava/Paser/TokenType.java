15310a09e26fea08aa046f2978045e595a92ec72
Tristan Elliott <>
2022-05-28 00:16:59 -0300

    starting parser

---
+++ src/main/java/com/example/LateNightJava/Paser/TokenType.java
@@ -0,0 +1,15 @@
+package com.example.LateNightJava.Paser;
+
+public enum TokenType {
+    //SINGLE CHARACTER TOKENS
+    HASH,
+
+    //LITERAL
+    STRING,NUMBER,
+
+    //KEYWORD
+    EOF
+
+
+
+}
